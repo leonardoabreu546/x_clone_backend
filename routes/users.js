7c96a7540202a3d384efe29881b94e2ee3165ca5
Leonardo <lcat140507@gmail.com>
2026-05-26 17:57:42 +0100

    adicionar rota sign up

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const authController = require('../controllers/authController');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+/*Registo*/
+router.post('/signup', authController.signup);
 
 module.exports = router;
