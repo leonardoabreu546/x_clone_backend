7eaf0f552cc14890c5752a30e5f7fe59df8a5a3d
Leonardo <lcat140507@gmail.com>
2026-05-26 19:23:29 +0100

    adicionar rota de comentários ao router

--- routes/tweets.js
+++ routes/tweets.js
@@ -1,12 +1,27 @@
 var express = require('express');
 var router = express.Router();
 const tweetController = require('../controllers/tweetController');
+const commentController = require('../controllers/commentController'); // <-- Nova importação
 const authMiddleware = require('../middleware/auth');
 
-// Rota para Ver o Feed (Qualquer pessoa pode ver)
+// ==========================================
+// ROTAS DE TWEETS
+// ==========================================
+
+// Rota para Ver o Feed 
 router.get('/', tweetController.getFeed);
 
-// Rota para Publicar Tweet (Protegida)
+// Rota para Publicar Tweet 
 router.post('/', authMiddleware, tweetController.create);
+
+// Rota para Dar ou Tirar Like 
+router.post('/:id/like', authMiddleware, tweetController.toggleLike);
+
+// ==========================================
+// ROTAS DE COMENTÁRIOS
+// ==========================================
+
+// Rota para Comentar num Tweet 
+router.post('/:id/comments', authMiddleware, commentController.create);
 
 module.exports = router;
