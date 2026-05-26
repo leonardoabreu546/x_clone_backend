be2242997a0bbf5834c038280b989df71d21d9b5
Leonardo <lcat140507@gmail.com>
2026-05-26 20:25:46 +0100

    criar automaticamente um perfil de utilizador

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { User } = require('../models');
+const { User, Profile } = require('../models');
 
 const authController = {
     // ==========================================
@@ -13,14 +13,20 @@ const authController = {
             const salt = await bcrypt.genSalt(10);
             const hashedPassword = await bcrypt.hash(password, salt);
 
+            // Criar o utilizador
             const newUser = await User.create({
                 username: username,
                 email: email,
                 password: hashedPassword
             });
 
+            // Criar o perfil vazio automaticamente (Relação 1:1)
+            await Profile.create({
+                fk_utilizador: newUser.id_utilizador
+            });
+
             return res.status(201).json({
-                message: 'Utilizador registado com sucesso!',
+                message: 'Utilizador registado com sucesso e Perfil criado!',
                 user: {
                     id: newUser.id_utilizador,
                     username: newUser.username,
